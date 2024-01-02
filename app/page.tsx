683000b032e27a02ac3a053f96c1c0d2a0d0fef2
MarcTheDev <admin@marcthedev.it>
2024-01-02 01:10:32 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,103 @@
-import Image from 'next/image'
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <Header />
+      <section className="bg-[#1f1f1f] pt-10 pb-10">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-md font-bold text-gray-300">WHAT IS MARCSYNC?</h1>
+          <p className="text-3xl font-bold mt-1 text-center">A simple Database Service</p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="flex justify-center mt-5">
+          <div className="flex flex-col w-[50rem] gap-3 max-w-[90%]">
+            <h3>With the help of MarcSync, you can store your data inside of a Document-Based Database. (MongoDB) <br /> This is a simple and easy way to store your data.</h3>
+            <p>MarcSync is especially useful for Roblox Developers, who want to store their data in a Database outside of the Roblox Network, or just want to store thier data inside of a Document-Based Database instead of a Key-Value-Based one. But it's also useful for Developers who simply want to store their data in a Database, but don't want to host it on their own.</p>
+            <p>With the help of MarcSync's powerful API, you can easily access and store your data from anywhere, at any time. MarcSync also offers clients for different programming languages including Luau (Roblox), NodeJS & Java which will make it even easier to access your data.</p>
+          </div>
+        </div>
+      </section>
+      <section className="bg-[#3d088a] pt-10 pb-10">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-md font-bold text-gray-300">WHY SHOULD I USE MARCSYNC?</h1>
+          <p className="text-3xl font-bold mt-1 text-center">It's simple, easy and free!</p>
+        </div>
+        <div className="flex justify-center mt-5 flex-col items-center">
+          <div className="grid grid-cols-3 w-[50rem] gap-3 max-w-[90%]">
+            <div className="flex flex-col gap-3">
+              <p className="font-bold">Simple</p>
+              <p>MarcSync is simple to use. You can easily store your data in a Database without having to host it on your own.</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <p className="font-bold">Easy</p>
+              <p>It's easy to use. Just create a Database, get an API Key and with it's simple API (and clients) you are good to go!</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <p className="font-bold">Free</p>
+              <p>MarcSync is 100% free to use. You can store your data in a Database for free, without having to pay anything.</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <p className="font-bold">Support</p>
+              <p>Need help? No problem! Just join our Discord Server and ask your question in the support channel.</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <p className="font-bold">Client Support</p>
+              <p>MarcSync offers clients for different programming languages including Luau (Roblox), NodeJS & Java and even more are coming soon!</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <p className="font-bold">Maintenence</p>
+              <p>MarcSync is up-to-date with the most latest modern security standers and is always getting new features and bug fixes.</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <p className="font-bold">Community</p>
+              <p>Join our Discord Server and be part of a growing community of Developers who use MarcSync.</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <p className="font-bold">Trusted by Developers</p>
+              <p>MarcSync is trusted by Developers like RoMart, a Roblox Game with over 3 Million Visits.</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <p className="font-bold">Uptime</p>
+              <p>MarcSync is hosted almost completely in the cloud, making sure that it's always online and ready to use.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="bg-[#1f1f1f] pt-10 pb-10">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-md font-bold text-gray-300">HOW DO I GET STARTED?</h1>
+          <p className="text-3xl font-bold mt-1 text-center">It's easy!</p>
+        </div>
+        <div className="flex justify-center mt-5">
+          <div className="flex flex-col w-[50rem] gap-3 max-w-[90%]">
+            <p>Head over to our <a className="text-[#ee1e7c] hover:underline" href="https://manage.marcsync.api.marcthedev.it" target="_blank" rel="noreferrer">Dashboard</a> and create a Database. After that, you can get your API Key by clicking on Manage {">"} Create Access Token.</p>
+            <p>Now you can start storing data in MarcSync! For more information on how to install and use MarcSync in your Roblox Game, check out our <a className="text-[#ee1e7c] hover:underline" href="https://docs.marcsync.dev" target="_blank" rel="noreferrer">Documentation</a>.</p>
+            <p>All of our clients are open-source and can be found on our GitHub:</p>
+            <ul className="list-disc list-inside ml-3">
+              <li><a className="text-[#ee1e7c] hover:underline" href="https://github.com/MarciiTheDev/marcsync-rbx-client" target="_blank" rel="noreferrer">Luau (Roblox)</a></li>
+              <li><a className="text-[#ee1e7c] hover:underline" href="https://github.com/marciithedev/marcsync-nodejs-client" target="_blank" rel="noreferrer">NodeJS</a></li>
+              <li><a className="text-[#ee1e7c] hover:underline" href="https://github.com/marcsync/marcsync-java-client" target="_blank" rel="noreferrer">Java</a></li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section className="bg-[#3d088a] pt-10 pb-10">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-md font-bold text-gray-300">CONTACT US</h1>
+          <p className="text-3xl font-bold mt-1 text-center">We are here to help!</p>
+        </div>
+        <div className="flex justify-center mt-5">
+          <div className="flex flex-col w-[50rem] gap-3 max-w-[90%]">
+            <p>If you need help, you can join our <a className="text-[#ee1e7c] hover:underline" href="https://discord.com/invite/pyysgrUeYE" target="_blank" rel="noreferrer">Discord Server</a> and ask your question in the support channel.</p>
+            <p>You can also contact us via email: <a className="text-[#ee1e7c] hover:underline" href="mailto:contact@marcsync.dev">contact@marcsync.dev</a></p>
+          </div>
+        </div>
+      </section>
+      <footer className="bg-[#1f1f1f] h-16">
+        <div className="flex flex-col items-center justify-center h-full">
+          <p className="text-gray-300">© 2022-2024 MarcSync Database</p>
+        </div>
+      </footer>
     </main>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                "hostname": "cdn.marcthedev.it",
+                "protocol": "https"
+            },
+            {
+                "hostname": "assets-global.website-files.com",
+                "protocol": "https"
+            }
+        ],
+    }
+}
 module.exports = nextConfig
